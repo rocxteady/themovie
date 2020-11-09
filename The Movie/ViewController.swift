83928e58e88186bb0d25f2f4c8005bc38d7f3823
Ulaş Sancak <ulas.sancak@bilyoner.com>
2020-11-09 23:05:28 +0300

    Initialized git

---
+++ The Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Movie
+//
+//  Created by Ulaş Sancak on 9.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
